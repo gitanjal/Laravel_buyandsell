0c94a5540c94f1da09bc1fa4ed68475a9bcbaef6
gitudrebel <gitudrebel94@gmail.com>
2021-08-20 23:22:02 +0530

    Create the view for product list

---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Buy And Sell</title>
+  </head>
+  <body>
+    <h1>Welcome to BnS</h1>
+    @for($i=0; $i<10; $i++)
+      Hello World<br>
+    @endfor
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/products',function(){
+  return view('products');
+});
+
 require __DIR__.'/auth.php';
