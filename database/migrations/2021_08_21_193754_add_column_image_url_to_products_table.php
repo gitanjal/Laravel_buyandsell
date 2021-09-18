3795b72ec4a540b165a10d709d8b7d9d9f969ac9
gitudrebel <gitudrebel94@gmail.com>
2021-09-18 13:17:11 +0530

    Assignment 15

--- database/migrations/2021_08_21_193754_add_column_image_url_to_products_table.php
+++ database/migrations/2021_08_21_193754_add_column_image_url_to_products_table.php
@@ -13,9 +13,17 @@ class AddColumnImageUrlToProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
-            //
-            $table->string('image_url');
+        /*
+        ***ASSIGNMENT***
+        Task 1: Are we calling the correct function below (create()!! we simply want to add a column to an existing table)?
+          Call the correct fucntion.
+        */
+        Schema::create('products', function (Blueprint $table) {
+            /*
+            ***ASSIGNMENT***
+            Task 2: Is $db the instance of Blueprint that we have inside this function? Replace with the correct one. 
+            */
+            $db->string('image_url');
         });
     }
 
